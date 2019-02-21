aa66128de72241bfbc586bd49273d00c8b698019
Travis Gee <geet084@gmail.com>
2019-02-21 13:09:58 -0700

    Add initial actions

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+
+export const isLoading = (bool) => ({
+  type: 'IS_LOADING',
+  isLoading: bool
+})
+
+export const hasErrored = (message) => ({
+  type: 'HAS_ERRORED',
+  message
+})
+
+export const fetchSuccess = (content) => ({
+  type: 'FETCH_SUCCESS',
+  content
+})
