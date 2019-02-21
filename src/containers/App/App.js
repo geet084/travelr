375b64e0a2126079cd248711492e1cdf146f4a11
Travis Gee <geet084@gmail.com>
2019-02-21 15:16:38 -0700

    Hook up connect/redux, move App into it's own container

---
+++ src/containers/App/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../../Main.scss';
+import { nasaApiKey } from '../../ApiKeys';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { fetchURL } from '../../thunks/fetchURL';
+
+class App extends Component {
+
+  componentDidMount = async () => {
+    const url = `https://cors-anywhere.herokuapp.com/https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}`
+    this.props.fetchURL(url)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          TRAVELR
+        </header>
+        <img src={this.props.content.url} alt=""/>
+      </div>
+    );
+  }
+}
+
+export const mapDispatchToProps = (dispatch) => ({
+  fetchURL: (url) => dispatch(fetchURL(url)),
+})
+
+export const mapStateToProps = (state) => ({
+  content: state.content,
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
