8fe23aadfe7280087682dcf1407cd62e23fb23ff
Travis Gee <geet084@gmail.com>
2019-02-22 09:26:31 -0700

    Format planet display info

--- src/containers/Display/Display.js
+++ src/containers/Display/Display.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 class Display extends Component {
   render() {
+    const { info, name } = this.props;
     return (
       <div>
-        {this.props.name}
+        {!info && name}
+        {info && <p>{'name  - ' + info.name}</p>}
+        {info && <p>{'temp  - ' + info.average_temperature}</p>}
+        {info && <p>{'dist  - ' + info.distance_from_sun}</p>}
+        {info && <p>{'day  - ' + info.length_of_day}</p>}
+        {info && <p>{'year  - ' + info.orbital_period}</p>}
       </div>
     )
   }
