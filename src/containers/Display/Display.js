f140c6ef677d9df58a0ba387454d1943a2214afa
Travis Gee <geet084@gmail.com>
2019-02-23 08:11:42 -0700

    Wire up image fetch and display

--- src/containers/Display/Display.js
+++ src/containers/Display/Display.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchImages } from '../../thunks';
 
 class Display extends Component {
+ 
+  componentDidMount = () => {
+    let { info, images, fetchImages } = this.props;
+    if (info && images !== []) fetchImages(info.imageIDs)
+  }
+  
   render() {
-    const { info, name } = this.props;
+    const { info, images } = this.props;
+
     return (
       <div>
-        {!info && name}
         {info && <p>{'name  - ' + info.name}</p>}
         {info && <p>{'temp  - ' + info.average_temperature}</p>}
         {info && <p>{'dist  - ' + info.distance_from_sun}</p>}
         {info && <p>{'day  - ' + info.length_of_day}</p>}
         {info && <p>{'year  - ' + info.orbital_period}</p>}
+        
+        {images.length !== 0 && <img className='imgs' src={images[0].items[0].href} alt="" />}
       </div>
     )
   }
 }
 
-export default Display;
+export const mapDispatchToProps = (dispatch) => ({
+  fetchImages: (arr) => dispatch(fetchImages(arr))
+})
+
+export const mapStateToProps = (state) => ({
+  images: state.images,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Display);
