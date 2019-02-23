d2da11c9045402a6db8fad08ebdc16156abc5c4a
Travis Gee <geet084@gmail.com>
2019-02-23 08:56:39 -0700

    Add counter elements to homepage

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
+import CountUp from 'react-countup';
 
 class Home extends Component {
+
+  counter = (start, stop, decimal = 0, suffix = ' miles per second') => {
+    return (
+      <CountUp
+        start={start} end={stop} decimals={decimal} suffix={suffix} duration='1000000' delay={0} useEasing={false} separator=','>
+        {({ countUpRef }) => <div><span ref={countUpRef} /></div>}
+      </CountUp>
+    )
+  }
+
   render() {
     const { url } = this.props;
+    let timeInSeconds = this.counter(0, 1000000, 1, ' seconds')
+    let earthSpin = this.counter(0, 280000, 1)
+    let earthOrbit = this.counter(0, 18500000)
+    let solarSystemOrbit = this.counter(0, 124450000)
+    let galaxyMovement = this.counter(0, 70000000)
+    let totalMovement = this.counter(0, 213230000)
+
     return (
       <div>
         <img className='apod-img' src={url} alt="apod" />
+        <header className="App-header">
+          <h4>HOW FAST YOU ARE MOVING RIGHT NOW</h4>
+          <h6>distance from new york to san francisco: <span>2,569 miles</span></h6>
+          <h6>distance from san francisco to paris: <span>5,560 miles</span> </h6>
+          <h6>distance from new york to australia: <span>10,512 miles</span></h6>
+          <h6>circumference of the earth: <span>24,901 miles</span></h6>
+          <h6>distance to the moon: <span>238,900 miles</span></h6>
 
+          <p>elapsed time {timeInSeconds}</p>
+          <p>Earth spinning {earthSpin}</p>
+          <p>Earth orbiting the Sun {earthOrbit}</p>
+          <p>Solar system orbiting in the galaxy {solarSystemOrbit}</p>
+          <p>Milky Way moving towards Andromeda {galaxyMovement}</p>
+          <p>Your total speed right now {totalMovement}</p>
+        </header>
       </div>
     )
   }
