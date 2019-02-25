98a651f33b72af9101406d7b31999c1e03d10bd3
Travis Gee <geet084@gmail.com>
2019-02-25 08:55:13 -0700

    Add NotFound tests

---
+++ src/components/NotFound/NotFound.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotFound from './NotFound';
+
+describe('NotFound', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NotFound />)
+  })
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+})
