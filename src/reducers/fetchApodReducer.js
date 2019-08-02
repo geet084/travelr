b235dd73b4ed4d4bd00a1a90b5485e2d28c88a15
Travis Gee <geet084@gmail.com>
2019-08-02 15:49:21 -0600

    Moves apod underneath images in store

--- src/reducers/fetchApodReducer.js
+++
@@ -1,8 +0,0 @@
-export const fetchApodReducer = (state = {}, action) => {
-  switch (action.type) {
-    case 'FETCH_APOD_SUCCESS':
-      return action.content;
-    default:
-      return state;
-  }
-}
