c720f123335015ceb40f28bffba1063927d262ff
Travis Gee <geet084@gmail.com>
2019-08-02 11:33:13 -0600

    Changes actions to be more semantic and concise

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,24 +13,19 @@ export const fetchApodSuccess = (content) => ({
   content
 })
 
-export const fetchPlanetsSuccess = (planets) => ({
-  type: 'FETCH_PLANETS_SUCCESS',
-  planets
+export const handleObjectsSuccess = (objects) => ({
+  type: 'HANDLE_OBJECTS_SUCCESS',
+  objects
 })
 
-export const fetchImagesSuccess = (images) => ({
-  type: 'FETCH_IMAGES_SUCCESS',
+export const handleImagesSuccess = (images) => ({
+  type: 'HANDLE_IMAGES_SUCCESS',
   images
 })
 
 export const setArrivalTime = (arrivalTime) => ({
   type: 'SET_ARRIVAL_TIME',
   arrivalTime
-})
-
-export const setBodies = (bodies) => ({
-  type: 'SET_BODIES',
-  bodies
 })
 
 export const setUserInfo = ({ userDate, elapsedDays }) => ({
