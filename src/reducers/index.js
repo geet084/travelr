09192eea896916c3a2fdcb89525a40cfc17a5979
Travis Gee <geet084@gmail.com>
2019-08-02 11:32:29 -0600

    Changes reducers to be more semantic and concise

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,19 +2,17 @@ import { combineReducers } from 'redux';
 import { isLoadingReducer } from './isLoadingReducer';
 import { hasErroredReducer } from './hasErroredReducer';
 import { fetchApodReducer } from './fetchApodReducer';
-import { fetchPlanetsReducer } from './fetchPlanetsReducer';
-import { fetchImagesReducer } from './fetchImagesReducer';
+import { handleObjectsReducer } from './handleObjectsReducer';
+import { handleImagesReducer } from './handleImagesReducer';
 import { setArrivalTimeReducer } from './setArrivalTimeReducer';
 import { setUserInfoReducer } from './setUserInfoReducer'
-import { setBodiesReducer } from './setBodiesReducer';
 
 export const rootReducer = combineReducers({
   isLoading: isLoadingReducer,
   hasErrored: hasErroredReducer,
   content: fetchApodReducer,
-  planets: fetchPlanetsReducer,
-  images: fetchImagesReducer,
+  objects: handleObjectsReducer,
+  images: handleImagesReducer,
   arrivalTime: setArrivalTimeReducer,
   userInfo: setUserInfoReducer,
-  bodies: setBodiesReducer,
 });
