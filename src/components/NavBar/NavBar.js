c63a4e3e27b8be3ad2e1fec29364b766e16e9c52
Travis Gee <geet084@gmail.com>
2019-08-04 12:32:42 -0600

    Refactors navBar to be dynamic based on objects from database

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,13 +4,28 @@ import Collapsible from 'react-collapsible';
 
 class NavBar extends Component {
   render() {
-    const objectNames = ['Sun', 'Moon', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
+    let { planets, moons, bodies } = this.props;
+    bodies = bodies.filter(name => name !== 'Sun')
+
     return (
       <nav className="nav-bar">
         <ul>
           <NavBtn name={'Home'} path='/' />
-          <Collapsible className="drop-down" trigger="Solar System">
-            {objectNames.map(name => <NavBtn name={name} path={`/objects/${name}`} key={name} />)}
+          <Collapsible className="drop-down" trigger="Solar System ˅" triggerWhenOpen="Solar System ˄">
+
+            <Collapsible className="drop-down" trigger="Sun & Planets ˅" triggerWhenOpen="Sun & Planets ˄">
+              <NavBtn name={'Sun'} path={'/objects/Sun'} key={'Sun'} />
+              {planets.map(name => <NavBtn name={name} path={`/objects/${name}`} key={name} />)}
+            </Collapsible>
+
+            <Collapsible className="drop-down" trigger="Moons ˅" triggerWhenOpen="Moons ˄">
+              {moons.map(name => <NavBtn name={name} path={`/moons/${name}`} key={name} />)}
+            </Collapsible>
+
+            <Collapsible className="drop-down" trigger="Other Bodies ˅" triggerWhenOpen="Other Bodies ˄">
+              {bodies.map(name => <NavBtn name={name} path={`/bodies/${name}`} key={name} />)}
+            </Collapsible>
+
           </Collapsible>
         </ul>
       </nav>
