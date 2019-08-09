0275b99b19f115d960af62ff43704173a0d1ed3c
Travis Gee <geet084@gmail.com>
2019-08-09 08:17:16 -0600

    Updates handleObjects test to match refactored thunk

--- src/thunks/__tests__/handleObjects.test.js
+++ src/thunks/__tests__/handleObjects.test.js
@@ -1,11 +1,18 @@
-import { isLoading, hasErrored, handleObjectsSuccess } from '../../actions'
+import { isLoading, hasErrored, handlePlanetsSuccess, handleMoonsSuccess, handleStarsSuccess, handleBodiesSuccess } from '../../actions'
 import { handleObjects } from '../handleObjects';
 
 describe('handleObjects', () => {
   const mockURL = 'https://someurl';
   const mockDispatch = jest.fn();
-  const mockObjects = [{}]
-  
+  const mockData = {
+    data: {
+      planets: [{ name: 'Earth', object_id: 5 }],
+      stars: [{ name: 'Sun', object_id: 1 }],
+      moons: [{ name: 'Moon', object_id: 8 }],
+      bodies: [{ name: 'Oort Cloud', object_id: 11 }],
+    }
+  };
+
   it('should dispatch isLoading(true)', () => {
     const thunk = handleObjects(mockURL);
     thunk(mockDispatch);
@@ -32,14 +39,47 @@ describe('handleObjects', () => {
     expect(mockDispatch).toHaveBeenCalledWith(isLoading(false))
   })
 
-  it('should dispatch handleObjectsSuccess with the retrieved planets', async () => {
+  it('should dispatch handlePlanetsSuccess with the retrieved planets', async () => {
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-      json: () => Promise.resolve(mockObjects),
+      json: () => Promise.resolve(mockData),
       ok: true
     }))
     const thunk = handleObjects(mockURL);
     await thunk(mockDispatch);
 
-    expect(mockDispatch).toHaveBeenCalledWith(handleObjectsSuccess(mockObjects))
+    expect(mockDispatch).toHaveBeenCalledWith(handlePlanetsSuccess(mockData.data.planets));
+  })
+
+  it('should dispatch handleStarsSuccess with the retrieved moons', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockData),
+      ok: true
+    }))
+    const thunk = handleObjects(mockURL);
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(handleStarsSuccess(mockData.data.stars));
+  })
+
+  it('should dispatch handleMoonsSuccess with the retrieved moons', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockData),
+      ok: true
+    }))
+    const thunk = handleObjects(mockURL);
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(handleMoonsSuccess(mockData.data.moons));
+  })
+
+  it('should dispatch handleBodiesSuccess with the retrieved moons', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockData),
+      ok: true
+    }))
+    const thunk = handleObjects(mockURL);
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(handleBodiesSuccess(mockData.data.bodies));
   })
 })
