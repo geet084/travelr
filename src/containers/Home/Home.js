9c68c73cfc9f34d70831be5ff25cae8723b3e714
Travis Gee <geet084@gmail.com>
2019-08-15 15:26:26 -0600

    Implements moment package for time and date use

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import CountUp from 'react-countup';
-import DateForm from '../../containers/DateForm/DateForm';
+import DateForm from '../../components/DateForm/DateForm';
 import Collapsible from 'react-collapsible';
 import PropTypes from 'prop-types';
+import moment from 'moment';
+import { connect } from 'react-redux';
 
 export class Home extends Component {
   constructor() {
     super();
     this.state = {
       today: '',
+      userDate: '',
+      elapsedTime: 0,
       timeInSeconds: 0,
       earthSpin: 0,
       earthOrbit: 0,
@@ -23,19 +27,32 @@ export class Home extends Component {
   }
 
   calculateDateAndTime = () => {
-    const now = Date.now() / 1000;
-    const timeInSeconds = now - (this.props.time / 1000);
-    const date = new Date();
-    date.setDate(date.getDate() - 1);
+    const arrivalTime = parseInt(this.props.arrivalTime);
+    const now = parseInt(moment().format('x'));
+    const elapsedTimeSinceArrival = (now - arrivalTime) / 1000;
+    const timeInSeconds = (arrivalTime === 0) ? .001 : elapsedTimeSinceArrival
+    const today = moment().format('llll');
 
     this.setState({
       timeInSeconds,
-      today: date.toDateString(),
+      today,
       earthSpin: (timeInSeconds * .28),
       earthOrbit: (timeInSeconds * 18.5),
       solarSystemOrbit: (timeInSeconds * 124.45),
       galaxyMovement: (timeInSeconds * 70),
       totalMovement: (timeInSeconds * 213.23),
+    });
+  }
+
+  updateUserDate = (enteredDate) => {
+    // TODO: set time to a local noon or midnight?
+    const { arrivalTime } = this.props;
+    const userDate = moment(enteredDate).format('llll');
+    const elapsedTime = moment(arrivalTime).diff(moment(enteredDate), 'x');
+
+    this.setState({
+      userDate,
+      elapsedTime
     });
   }
 
@@ -53,11 +70,15 @@ export class Home extends Component {
     const { userDate, elapsedDays } = this.props.userInfo;
     let { today, timeInSeconds, earthSpin, earthOrbit, solarSystemOrbit, galaxyMovement, totalMovement, } = this.state;
 
-    const days = Math.floor(elapsedDays)
-    const hours = Math.floor(((elapsedDays - days) * 24) - 7)
-    const total = ((days + (hours / 24)) * 24000);
+    let userEnteredDate = moment(this.state.userDate)
+    let arrivalDate = moment(this.state.today)
 
-    let num = this.counter((total + totalMovement), (total + totalMovement + 213230000), 1);
+    const hasUserDate = this.state.userDate !== '';
+    const days = hasUserDate ? arrivalDate.diff(userEnteredDate, 'days') : 0
+    const hours = hasUserDate ? arrivalDate.diff(userEnteredDate, 'hours') - (days * 24) : 0
+    const total = ((days + (hours / 24)) * 18424000);
+
+    const num = this.counter((total + totalMovement), (total + totalMovement + 213230000), 1);
 
     return (
       <div className='home'>
@@ -68,15 +89,15 @@ export class Home extends Component {
             <h3 className="test">How far have you traveled this week?</h3>
             <h3 className="test input-date">
               Enter a date to find out:
-              <DateForm className="test" key={today} today={today} />
+              <DateForm className="test" key={this.state.userDate} today={today} updateUserDate={this.updateUserDate} />
             </h3>
             {
-              elapsedDays !== 0 &&
+              days !== 0 &&
               <div>
-                <p className="test">Time elapsed since {userDate}: </p>
+                <p className="test">Time elapsed since {this.state.userDate}: </p>
                 <p className="test">
-                  <span>{days.toLocaleString()} days and </span>
-                  <span> ~{hours.toLocaleString()} hours</span>
+                  <span>{days} days and </span>
+                  <span> ~{hours} hours</span>
                 </p>
                 <p className="test">Approx. total miles you've gone in that time: </p>
                 <p className="test user-dist">{num}</p>
@@ -85,9 +106,8 @@ export class Home extends Component {
           </div>
         </header>
         {
-          userInfo && userInfo.elapsedDays !== 0 &&
+          days !== 0 &&
           <Collapsible className="test extra" trigger="Show More">
-
             <div className='stats'>
               <h4 className="test">HOW FAST YOU ARE MOVING RIGHT NOW</h4>
               <h6 className="test">distance from new york to san francisco: <span>2,569 miles</span></h6>
@@ -121,6 +141,11 @@ export class Home extends Component {
   }
 }
 
+export const mapStateToProps = (state) => ({
+  arrivalTime: state.arrivalTime,
+  userInfo: state.userInfo,
+})
+
 Home.propTypes = {
   time: PropTypes.number,
   url: PropTypes.string,
@@ -133,4 +158,4 @@ Home.defaultProps = {
   userInfo: {},
 }
 
-export default Home;
+export default connect(mapStateToProps)(Home);
