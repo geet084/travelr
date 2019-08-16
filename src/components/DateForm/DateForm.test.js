f86fcb6ea0e7aaff1999e3d309da82b340a90fff
Travis Gee <geet084@gmail.com>
2019-08-16 13:00:27 -0600

    Adds build date inputs method test to date form

--- src/components/DateForm/DateForm.test.js
+++ src/components/DateForm/DateForm.test.js
@@ -5,16 +5,16 @@ import { DateForm } from './DateForm';
 describe('DateForm', () => {
   let wrapper;
   const mockUpdateUserDate = jest.fn();
-  
+
   beforeEach(() => {
     wrapper = shallow(<DateForm updateUserDate={mockUpdateUserDate} />)
   });
-  
+
   describe('initial state', () => {
     it('should match snapshot', () => {
       expect(wrapper).toMatchSnapshot();
     });
-    
+
     it('should have a default state', () => {
       const expected = {
         month: '',
@@ -24,18 +24,18 @@ describe('DateForm', () => {
       expect(wrapper.state()).toEqual(expected);
     });
   });
-  
+
   describe('componentWillUpdate', () => {
     const mockMonthEvent = { target: { id: 'month', value: '07', min: '01', max: '12' } };
     const mockDayEvent = { target: { id: 'day', value: '08', min: '01', max: '31' } };
-    const mockYearEvent = { target: { id: 'year', value: '1999', min: '0100', max: '2999' } }; 
-    
+    const mockYearEvent = { target: { id: 'year', value: '1999', min: '0100', max: '2999' } };
+
     it('should NOT call updateUserDate if date is not complete', () => {
       wrapper.find('#month').simulate('change', mockMonthEvent);
-      
+
       expect(mockUpdateUserDate).toHaveBeenCalledTimes(0);
     });
-    
+
     it('should call updateUserDate if the date IS complete', () => {
       const expected = '07-08-1999';
 
@@ -46,4 +46,23 @@ describe('DateForm', () => {
       expect(mockUpdateUserDate).toHaveBeenCalledWith(expected);
     });
   });
-})
+
+  describe('buildDateInputs', () => {
+    it('should build date inputs with provided date types object', () => {
+      const mockDateTypes = {
+        month: { placeholder: 'mm', length: 2, min: '01', max: '12' },
+        day: { placeholder: 'dddd', length: 5, min: '15', max: '86' },
+        year: { placeholder: 'yyyy', length: 4, min: '0100', max: '2999' }
+      }
+      const result = wrapper.instance().buildDateInputs(mockDateTypes);
+
+      expect(result.length).toEqual(3);
+      expect(result[1].props.id).toEqual('day');
+      expect(result[1].props.max).toEqual('86');
+      expect(result[1].props.maxLength).toEqual(5);
+      expect(result[1].props.min).toEqual('15');
+      expect(result[1].props.placeholder).toEqual('dddd');
+      expect(result[1].props.value).toEqual('');
+    });
+  });
+});
