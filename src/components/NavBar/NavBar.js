2cb8817572a8f9f8be8ecfef9d500157cb68f0e8
Travis Gee <geet084@gmail.com>
2019-08-23 18:37:51 -0600

    Refactors NavBar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,40 +3,47 @@ import NavBtn from '../NavBtn/NavBtn';
 import Collapsible from 'react-collapsible';
 
 export class NavBar extends Component {
-  setCategoryTitle = () => {
+  
+  getCategoryTitles = () => {
     return Object.keys(this.props).map(key => {
       const updatedKey = key.split('');
       updatedKey[0] = updatedKey[0].toUpperCase();
+      
       return updatedKey.join('');
     });
   }
 
-  populateCategory = (pathname) => {
-    return this.props[pathname].map(name => <NavBtn name={name} path={`/${pathname}/${name}`} key={pathname + name} />)
-  }
-
-  render() {
-    const navCategories = this.setCategoryTitle();
-    const navItems = navCategories.map(title => {
-      let pathname = title.toLowerCase()
+  populateNavItems = (navCategories) => {
+    return navCategories.map(title => {
+      const pathname = title.toLowerCase();
+      const navItemsInCategory = this.props[pathname].map(name => {
+        return <NavBtn name={name} path={`/${pathname}/${name}`} key={pathname + name} />
+      })
 
       return <Collapsible
         key={pathname}
         className="drop-down"
         trigger={`${title} ˅`}
-        triggerWhenOpen={`${title} ˄`} >
-        {this.populateCategory(pathname)}
-      </Collapsible>
+        triggerWhenOpen={`${title} ˄`}
+      >
+        {navItemsInCategory}
+      </Collapsible>;
     });
+  }
+
+  render() {
+    const navCategoryTitles = this.getCategoryTitles();
+    const navItems = this.populateNavItems(navCategoryTitles);
 
     return (
       <nav className="nav-bar">
         <ul>
-          <NavBtn name={'Home'} path='/' />
+          <NavBtn key={'home'} name={'Home'} path='/' />
           <Collapsible
             className="drop-down"
             trigger="Solar System ˅"
-            triggerWhenOpen="Solar System ˄" >
+            triggerWhenOpen="Solar System ˄"
+          >
             {navItems}
           </Collapsible>
         </ul>
--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NavBar from './NavBar';
-import NavBtn from '../NavBtn/NavBtn';
 
 describe('NavBar', () => {
   let wrapper;
+  let mockProps;
 
   beforeEach(() => {
-    const mockProps = {
+    mockProps = {
       planets: ['name1', 'name2'],
       moons: ['name1', 'name2', 'name3'],
       stars: ['name8'],
@@ -30,20 +30,23 @@ describe('NavBar', () => {
   it('should match snapshout with props', () => {
     expect(wrapper).toMatchSnapshot();
   });
-  
-  it('should set category title from props', () => {
+
+  it('should get category title from props', () => {
     const expected = ["Planets", "Moons", "Stars", "Bodies"];
-    const result = wrapper.instance().setCategoryTitle();
+    const result = wrapper.instance().getCategoryTitles();
 
     expect(result).toEqual(expected);
   });
 
-  it('should populate the category with items given a category path', () => {
-    const expected = wrapper.instance().props.moons.length;
-    const mockPath = 'moons';
+  it('should populate the nav bar with categories and items', () => {
+    const moonIndex = Object.keys(mockProps).indexOf('moons');
+    const moons = Object.keys(mockProps)[moonIndex]
+    const expected = wrapper.instance().props[moons].length;
 
-    const result = wrapper.instance().populateCategory(mockPath);
-
-    expect(result).toHaveLength(expected);
+    const mockCategories = ["Planets", "Moons", "Stars", "Bodies"];
+    
+    const result = wrapper.instance().populateNavItems(mockCategories);
+    
+    expect(result[moonIndex].props.children).toHaveLength(expected);
   });
 })
