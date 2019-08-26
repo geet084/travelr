83b450eb8bfc5f334a02ca3c1b6ab8934e70c1a5
Travis Gee <geet084@gmail.com>
2019-08-26 17:14:23 -0600

    Refactors NavBar to be a stateless component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,55 +1,58 @@
-import React, { Component } from 'react'
+import React from 'react';
 import NavBtn from '../NavBtn/NavBtn';
 import Collapsible from 'react-collapsible';
 
-export class NavBar extends Component {
-  
-  getCategoryTitles = () => {
-    return Object.keys(this.props).map(key => {
-      const updatedKey = key.split('');
-      updatedKey[0] = updatedKey[0].toUpperCase();
-      
-      return updatedKey.join('');
-    });
-  }
+export const getCategoryTitles = (props) => {
+  return Object.keys(props).map(key => {
+    const updatedKey = key.split('');
+    updatedKey[0] = updatedKey[0].toUpperCase();
+    
+    return updatedKey.join('');
+  });
+}
 
-  populateNavItems = (navCategories) => {
-    return navCategories.map(title => {
-      const pathname = title.toLowerCase();
-      const navItemsInCategory = this.props[pathname].map(name => {
-        return <NavBtn name={name} path={`/${pathname}/${name}`} key={pathname + name} />
-      })
+export const getNavCategoryItems = (props, title) => {
+  const pathname = title.toLowerCase();
 
-      return <Collapsible
-        key={pathname}
-        className="drop-down"
-        trigger={`${title} ˅`}
-        triggerWhenOpen={`${title} ˄`}
-      >
-        {navItemsInCategory}
-      </Collapsible>;
-    });
-  }
+  return props[pathname].map(name => {
+    return <NavBtn name={name} path={`/${pathname}/${name}`} key={pathname + name} />;
+  });
+}
 
-  render() {
-    const navCategoryTitles = this.getCategoryTitles();
-    const navItems = this.populateNavItems(navCategoryTitles);
+export const populateNavItems = (props) => {
+  const navCategories = getCategoryTitles(props);
 
-    return (
-      <nav className="nav-bar">
-        <ul>
-          <NavBtn key={'home'} name={'Home'} path='/' />
-          <Collapsible
-            className="drop-down"
-            trigger="Solar System ˅"
-            triggerWhenOpen="Solar System ˄"
-          >
-            {navItems}
-          </Collapsible>
-        </ul>
-      </nav>
-    )
-  }
+  return navCategories.map(title => {
+    const navItemsInCategory = getNavCategoryItems(props, title)
+    
+    return <Collapsible
+      key={title}
+      className="drop-down"
+      trigger={`${title} ˅`}
+      triggerWhenOpen={`${title} ˄`}
+    >
+      {navItemsInCategory}
+    </Collapsible>;
+  });
+}
+
+export const NavBar = (props) => {
+  const navItems = populateNavItems(props);
+
+  return (
+    <nav className="nav-bar">
+      <ul>
+        <NavBtn key={'home'} name={'Home'} path='/' />
+        <Collapsible
+          className="drop-down"
+          trigger="Solar System ˅"
+          triggerWhenOpen="Solar System ˄"
+        >
+          {navItems}
+        </Collapsible>
+      </ul>
+    </nav>
+  )
 }
 
 export default NavBar;
